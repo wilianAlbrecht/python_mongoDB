ca45e8f96ce26dc14dcdb64ea3b2dfa00921d8ef
wilianAlbrecht <wilian0012@hotmail.com>
2024-09-17 16:13:16 -0400

    primeira conexão

--- raw.py
+++ raw.py
@@ -1,22 +1,28 @@
 from pymongo import MongoClient
 
-connection_string = "mongodb://root:root@localhost:27017/?authSource=admin"
+# connection_string = "mongodb://user:123@localhost:27017/" #?authSource=admin
+
+connection_string = "mongodb://user:123@localhost:27017/?authSource=teste_db"
+
 client = MongoClient(connection_string)
-db_connection = client["myBase"]
+db_connection = client["teste_db"]
 
 # print(db_connection)
 
-collection = db_connection.get_collection("myCollection")
+collection = db_connection.get_collection("teste_collection")
 
-# print(collection)
+print(collection)
 
 search_filter = {"ola": "mundo"}
 
-# response = collection.find(search_filter)
+response = collection.find(search_filter)
 
-# print(response)
+print(response)
 
-collection.insert_one({
-    "Estou": "Inserindo",
-    "numeros": [123, 456, 789]
-})
+for r in response:
+    print(r)
+
+# collection.insert_one({
+#     "Estou": "Inserindo",
+#     "numeros": [123, 456, 789]
+# })
